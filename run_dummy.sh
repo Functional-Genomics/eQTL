cbe8a59b50d09ad2e260a4a1abf23e10643bd513
Nuno Fonseca <firstname.lastname@gmail.com>
2015-09-26 11:40:55 +0100

    working dummy example

---
+++ run_dummy.sh
@@ -0,0 +1,1 @@
+./scripts/eqtl_pipeline dna_rna_mapfile=test/dummy/map_file.tsv conf=t.conf name=aaaa expr_matrix=test/dummy/expr.tsv vcfs=test/dummy/v1.vcf.gz gtf_file=test/dummy/annot.gtf chromosomes="1 2" -n 
