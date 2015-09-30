080f9f6ce31d5fbb3464f22af161fe796693cad0
clody23 <claudia.calabrese23@gmail.com>
2015-09-30 18:24:24 +0100

    fixed

--- scripts/pheno_preprocessing.py
+++ scripts/pheno_preprocessing.py
@@ -3,6 +3,7 @@ import getopt, sys, os
 import h5py
 import csv
 import numpy as np
+import pandas as ps
 
 def usage():
 	print """
@@ -25,33 +26,31 @@ phenofileout=sys.argv[3]
 ### reading (Pheno reading should be reviewed)
 mapfile=np.loadtxt(mapfile, delimiter='\t', dtype='S50')
 
-GE=open(file,'r').readlines()
-GE=[i.strip().split('\t') for i in GE]
-GE[0]=map(lambda x:x.replace('"',''), GE[0])
+GE = pd.read_csv(phenofile, delimiter = "\t")
 
+header = GE.colums.values.tolist()
+#header = map(lambda x:x.replace('"',''), header)
 rnafiles=mapfile[1:,1].tolist()
-n=map(lambda x:GE[0].index(x),rnafiles)
+n=map(lambda x:header.index(x),rnafiles)
 n.insert(0,0)
 
 #grep only columns from the GEarray matching the RNA samples with a correspondent VCF analysis IDs.
-GEarray=np.array(GE)
-GEarraysliced=np.take(GEarray, n, axis=1)
-annot=GEarraysliced.shape[0]
-nsamples=GEarraysliced.shape[1]-1
+GEsliced=GE.take(n, axis=1)
 
+#get shape of the matrix of genes
+annot=GEliced.shape[0]-1
+nsamples=GEsliced.shape[1]-1
 print 'matrix with {0} genes and {1} samples'.format(annot,nsamples)
 
+#transpose the matrix
+GEt = GEsliced.T
 #take indexes to sort the gene expression table based on VCF samples order
 #indexes_again=map(lambda x:GEarraysliced[0].tolist().index(x), samples)
 #GEarraysliced[:,range(1,nsamples+1)]=GEarraysliced[:,indexes_again]
-GEt=GEarraysliced.transpose()
-GEt[0][0]='Samples'
-#write the new CSV file with Gene expression 
-with open(phenofileout, 'w') as csvfile:
-    GEwriter = csv.writer(csvfile, delimiter='\t', quotechar='|',quoting=csv.QUOTE_MINIMAL)
-    for row in GEt:
-        GEwriter.writerow(row)
 
+#write the new CSV file with Gene expression 
+phenofileout = open(phenofileout, 'w')
+Get.to_csv(phenofileout, sep = '\t', header = False)
 phenofileout.close()
 
 
