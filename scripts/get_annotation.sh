cc116835ca4c93dd3461c03ce2c9a09e320651f2
Nuno Fonseca <firstname.lastname@gmail.com>
2015-10-06 22:16:40 +0100

    catch errors

--- scripts/get_annotation.sh
+++ scripts/get_annotation.sh
@@ -1,16 +1,32 @@
 #!/bin/bash
 
-usage()
-{
-	USAGE=""" 
-	get_annotation.sh <infile.gtf> <outfile.csv> """
+function usage() {
 
-	echo $USAGE
+    cat<<EOF    
+Usage: get_annotation.sh <infile.gtf> <outfile.csv>
+EOF
+
 }
 
 gtf=$1
-csv=$2
+out=$2
 
-awk '$3 == "gene" {print $1,$4,$5,$10}' $gtf | sed 's/chr//g' | sed 's/"//g' | sed 's/\;//g' | sed 's/ /\t/g' > $csv
+if [ "$gtf-" == "-" ]; then
+    echo "ERROR: missing infile.gtf"
+    usage
+    exit 1
+fi
+if [ ! -e "$gtf"  ]; then
+    echo "ERROR: $gtf file not found"
+    exit 1
+fi
+if [ "$out-" == "-" ]; then
+    echo "ERROR: missing outfile"
+    usage
+    exit 1
+fi
 
+awk '$3 == "gene" {print $1,$4,$5,$10}' $gtf | sed "s/chr//g;s/\;//g;s/ /\t/g" > $out
+
+exit 0
 
