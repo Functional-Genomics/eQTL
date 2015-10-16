a46907b6016b9ae2d17f15e1a42ab51999d97285
Nuno Fonseca <firstname.lastname@gmail.com>
2015-10-16 15:41:00 +0100

    Minor fixes

--- docker/build_docker_image.sh
+++ docker/build_docker_image.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-cat ../Dockerfile| sudo docker build --rm --tag fedora/eqtl:latest -
+cat ../Dockerfile| sudo docker build --tag fedora/eqtl:latest -
+#cat ../Dockerfile| sudo docker build --rm --tag fedora/eqtl:latest -
 
+exit 0
