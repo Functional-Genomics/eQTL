2acf4ddc590875c01f168ebbf48dfe466cdeb0c3
Nuno Fonseca <firstname.lastname@gmail.com>
2015-10-18 23:07:00 +0100

    add comment

--- test/run_ex2.sh
+++ test/run_ex2.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+# to override the corr_method run
+# run_ex2.sh corr_method=panama
+# or
+# run_ex2.sh corr_method=peer
 eqtl_pipeline conf=ex2.conf $*
-# there is an issue with test.HG00097.vcf.gz 
+
