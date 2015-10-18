9da7b38c1d0e1728b8b513421e6c59b6bd50ffd8
clody23 <claudia.calabrese23@gmail.com>
2015-10-18 01:48:30 +0100

    fix Kpop calculation and SNPs matrix imputation

--- scripts/geno_preprocessing.py
+++ scripts/geno_preprocessing.py
@@ -22,21 +22,19 @@ def impute_missing(matrix):
 		else:
 			mean = matrix[~Imiss,i].mean()
 			matrix[Imiss,i] = mean
-	return 'Done'
+	return matrix
 
 #calculate kinship 
-def corr_matrix(matrix, genofile):
+def corr_matrix(matrix):
 
 	'''this function standardises the geno data by centering SNPs to mean 0, with unit variance. 
 	Then it calculates matrix of correlation N x N, using matrix of N samples x G genotypes ''' 
 	matrix -= matrix.mean(0)
 	matrix /= matrix.std(0)
+	nan=SP.isnan(matrix)
+	matrix[nan]=0.0
 	K = SP.dot(matrix,matrix.T)
-	##populate Geno dataset with kinship matrix
-	Kshape=K.shape
-	kpop=genofile.create_dataset('genotype/Kpop',Kshape,dtype="float64")
-	kpop[...]=K[:]
-	return 'Done'
+	return matrix,K
 
 if __name__ == '__main__':
         
@@ -53,13 +51,22 @@ if __name__ == '__main__':
         	sys.stderr.write("ERROR: file "+geno+" not found\n")
         	sys.exit(1)
 
-	geno = h5py.File(geno)
-	geno_matrix = geno['genotype/matrix'][:]
-	impute_missing(geno_matrix)
-	corr_matrix(geno_matrix, geno)
-	print 'check if keys are ok'
-	for i in geno.keys():
-    		for z in geno[i].keys():
-        		print z
+	genofile = h5py.File(geno)
+	geno_matrix = genofile['genotype/matrix'][:]
+	#substitute missing values with mean of not-missing 
+	genofile['genotype/matrix'][:] = impute_missing(geno_matrix)
+	#standardise matrix of SNPs and get Kpop
+	matrix,K = corr_matrix(geno_matrix)
+	#substitute matrix of imputed values with standardise matrix
+	genofile['genotype/matrix'][:]=matrix[:]
+	#populate dataset with Kpop
+        Kshape=K.shape
+        kpop=genofile.create_dataset('genotype/Kpop',Kshape,dtype="float64")
+        kpop[...]=K[:]
+	print 'check if file {0} has 4 keys: Kpop, col_header, row_header, matrix\n'.format(geno)
+	for dset in genofile.keys():
+    		for key in genofile[dset].keys():
+        		print key
+	print 'OK\n'
 
-	geno.close()
+	genofile.close()
