e819023ff2dbdb5f6c41a165810342db38239495
Nuno Fonseca <firstname.lastname@gmail.com>
2015-10-20 23:22:24 +0100

    fixes

--- scripts/eqtl_pipeline_lsf.sh
+++ scripts/eqtl_pipeline_lsf.sh
@@ -12,6 +12,11 @@ if [ "$THREADS-" = "-" ]; then
     THREADS=1
 fi
 
+if [ "$QUEUE-" = "-" ]; then
+    echo "QUEUE not defined. please run: export QUEUE=<lsf_queue_to_use>" 
+    exit 1
+fi
+
 
 function stop_job {
     if [ "$DEBUG-" == "1-" ]; then
@@ -129,11 +134,14 @@ targets=`eqtl_pipeline $* targets5 | tail -n 1`
 submit_jobs eqtl5_$JOBNAME_SUF "eqtl4_$JOBNAME_SUF[*]" eqtl_pipeline $ARGS
 
 targets=`eqtl_pipeline $* targets6 | tail -n 1`
-submit_jobs eqtl6_$JOBNAME_SUF "eqtl4_$JOBNAME_SUF[*]" eqtl_pipeline $ARGS
+submit_jobs eqtl6_$JOBNAME_SUF "eqtl5_$JOBNAME_SUF[*]" eqtl_pipeline $ARGS
+
+targets=`eqtl_pipeline $* targets7 | tail -n 1`
+submit_jobs eqtl7_$JOBNAME_SUF "eqtl6_$JOBNAME_SUF[*]" eqtl_pipeline $ARGS
 
 # final job
 targets=step4
-submit_job_get_email  eqtl7_$JOBNAME_SUF "eqtl6_$JOBNAME_SUF[*]" eqtl_pipeline $ARGS
+submit_job_get_email  eqtl8_$JOBNAME_SUF "eqtl7_$JOBNAME_SUF[*]" eqtl_pipeline $ARGS
 
 resume_job "eqtl0_$JOBNAME_SUF"
 exit 0
