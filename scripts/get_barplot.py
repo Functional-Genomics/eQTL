d5d5ed0afd9953924dc98a4268598cb638c49ddf
clody23 <claudia.calabrese23@gmail.com>
2015-10-30 00:05:55 +0000

    fix space between bars.TODO:increase space

--- scripts/get_barplot.py
+++ scripts/get_barplot.py
@@ -99,8 +99,8 @@ if __name__ == '__main__':
         #'size'   : '15'}
 
 	#rc('font', **font)  # pass in the font dict as kwargs
-	#index = np.arange(n_groups)
-	index = np.arange(0, n_groups * 2, 2)
+	index = np.arange(n_groups)
+	#index = np.arange(0, n_groups * 2, 2)
 	bar_width = 0.2
 
 	opacity = 0.4
