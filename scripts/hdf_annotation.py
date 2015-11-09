eafad21aeb672dbbd1c4e6f745ad4addbd9f5b3d
clody23 <claudia.calabrese23@gmail.com>
2015-11-09 16:28:39 +0000

    fix big bug

--- scripts/hdf_annotation.py
+++ scripts/hdf_annotation.py
@@ -13,8 +13,8 @@ hdf_annotation.py <annotation.tsv> <pheno.hdf5> '''
 
 #check number of arguments
 if len(sys.argv[1:]) != 2 :
-	sys.stderr.write("ERROR: full set of parameters not provided \n")
 	usage()
+	sys.stderr.write("ERROR: full set of parameters not provided \n")
 	sys.exit(1)
 
 
@@ -34,6 +34,10 @@ elif os.path.isfile(phenofile) != True:
 annotation = sp.loadtxt(annotation, delimiter='\t', dtype='S100')
 pheno=h5py.File(phenofile) #appending mode
 
+#number of genes in annotation
+
+print 'number of genes in annotation file {0}'.format(len(annotation))
+
 #strip unwanted char from genes in annotation array
 annotation[:,3] = sp.char.replace(annotation[:,3], '"','')
 
@@ -41,17 +45,40 @@ annotation[:,3] = sp.char.replace(annotation[:,3], '"','')
 booleanvector=sp.in1d(annotation[:,3][:], pheno['phenotype/col_header/phenotype_ID'][:].tolist()) #catch warnings if hdf5 dataset does not exist
 #get shape
 shape=annotation[sp.where(booleanvector)[0]][:,0].shape
+#get dataframe from annotation
+
+annotation=annotation[sp.where(booleanvector)[0]]
+
+print 'number of genes kept from annotation {0}'.format(len(annotation))
+
+annotation_list=annotation[:,3].tolist()
+gene_list=pheno['phenotype/col_header/phenotype_ID'][:].tolist()
+idx=[]
+for gene in gene_list:
+	i=annotation_list.index(gene)
+	idx.append(i)
+
+annotation=annotation[idx,:]
+
 #chrom
 dataset=pheno.create_dataset('phenotype/chrom', shape, dtype='S10') #same here
-dataset[...]=annotation[sp.where(booleanvector)[0]][:,0]
+dataset[...]=annotation[:,0][:]
 #start
-start=(annotation[sp.where(booleanvector)[0]][:,1]).astype('int64')
+start=(annotation[:,1]).astype('int64')
 dataset=pheno.create_dataset('phenotype/start', shape, dtype='int64') #same here
 dataset[...]=start[:]
 #end
-end=(annotation[sp.where(booleanvector)[0]][:,2]).astype('int64')
+end=(annotation[:,2]).astype('int64')
 dataset=pheno.create_dataset('phenotype/end', shape, dtype='int64') #same here
 dataset[...]=end[:]
+
+#check annotation in gene expression file
+
+print 'Chr\tStart\tEnd\tGene'
+
+for x in xrange(10):
+	o='\t'.join(annotation[x])
+	print o
 
 #check keys (maybe useless)
 print 'check if {0} has 6 datasets: matrix, col_header, row_header, chrom, start, end'.format(phenofile)
