25f160c3c774d6ca13fb1c0206670e1be340ea30
clody23 <claudia.calabrese23@gmail.com>
2016-01-06 16:32:25 +0000

    filter genotypes in mqtl_pipeline

---
+++ scripts/geno_filtering.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python env
+
+
+import sys,os
+import pandas as pd
+import scipy as sp
+
+def usage():
+	print '''
+
+Usage:
+geno_filtering.py <var.tsv> <threshold> <filtered_matrix.tsv>
+'''
+
+if len(sys.argv[1:]) < 3:
+	usage()
+	sys.stderr.write('\nERROR: missing parameter\n')
+	sys.exit(1)
+
+file1,threshold,out = sys.argv[1:]
+
+
+if os.path.isfile(file1) != True:
+	sys.stderr.write('\nERROR: file '+file1+' not found\n')
+	sys.exit(1)
+
+try:
+	threshold = int(threshold)
+except:
+	usage()
+	sys.stderr.write('\nERROR: threshold must be int\n')
+	sys.exit(1)
+
+matrix = pd.DataFrame.from_csv(file1,sep='\t')
+
+#select rows based on number of samples with at least 1 event
+
+filt_matrix = matrix[matrix.gt(0,axis='rows').sum(1) >= threshold]
+
+filt_matrix.to_csv(out,sep='\t',header=True,index=True)
+
+sys.exit(0)
+
