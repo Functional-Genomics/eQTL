6774cd8bee81715f7b2495caad888e3bf702f2fe
clody23 <claudia.calabrese23@gmail.com>
2016-01-12 16:05:33 +0000

    generate NaN Kpop.TODO: add a parameter to make file

--- scripts/geno_preprocessing.py
+++ scripts/geno_preprocessing.py
@@ -6,8 +6,11 @@ import h5py
 
 #usage
 def usage():
-	print """ This script inputes missing values based on the mean of non-missing genotypes .
-Usage: geno_preprocessing.py hdf5_file"""
+	print """ This script inputes missing values based on the mean of non-missing genotypes and calculates genetic kinship. If skip_kinship parameter is provided it generates a Kpop matrix of NaN.
+
+Usage: geno_preprocessing.py <hdf5_file> 
+
+Optional parameters: <skip_kinship> """
 
 #impute missing genotypes 
 def impute_missing(matrix):
@@ -25,24 +28,31 @@ def impute_missing(matrix):
 	return matrix
 
 #calculate kinship 
-def corr_matrix(matrix):
+def corr_matrix(matrix,skip_kinship):
 
-	'''this function standardises the geno data by centering SNPs to mean 0, with unit variance. 
-	Then it calculates matrix of correlation N x N, using matrix of N samples x G genotypes ''' 
-	matrix -= matrix.mean(0)
-	matrix /= matrix.std(0)
-	nan=SP.isnan(matrix)
-	matrix[nan]=0.0
-	K = SP.dot(matrix,matrix.T)
+	'''if calculate_kinship is False, this function standardises the geno data by centering SNPs to mean 0, with unit variance. Then it calculates matrix of correlation N x N, using matrix of N samples x G genotypes. If skip_kinship is True, it generates a N x N matrix of NaN '''
+	if skip_kinship == False: 
+		matrix -= matrix.mean(0)
+		matrix /= matrix.std(0)
+		nan=SP.isnan(matrix)
+		matrix[nan]=0.0
+		K = SP.dot(matrix,matrix.T)
+	else:
+		#if kinship is not requested
+		shape=matrix.shape[0]
+		K = SP.empty((shape,shape))
+		K[:] = SP.NAN
 	return matrix,K
+
 
 if __name__ == '__main__':
         
+	skip_kinship=False
 
 	#check argument
-	if len(sys.argv[1:]) != 1:
-                sys.stderr.write("ERROR: geno hdf5 file not provided\n")
-	        usage()
+	if len(sys.argv[1:]) < 1:
+		usage()
+                sys.stderr.write("ERROR: missing parameter\n")
         	sys.exit(1)
 
 	geno = sys.argv[1]
@@ -50,13 +60,19 @@ if __name__ == '__main__':
 	if os.path.isfile(geno) != True:
         	sys.stderr.write("ERROR: file "+geno+" not found\n")
         	sys.exit(1)
-
+	
+	#import genotype
 	genofile = h5py.File(geno)
 	geno_matrix = genofile['genotype/matrix'][:]
 	#substitute missing values with mean of not-missing 
 	genofile['genotype/matrix'][:] = impute_missing(geno_matrix)
-	#standardise matrix of SNPs and get Kpop
-	matrix,K = corr_matrix(geno_matrix)
+	#standardise matrix of SNPs and get Kpop or generate a NaN Kpop matrix if requested
+	if len(sys.argv[1:]) == 2:
+		print 'generating a matrix of NaN as Kpop'
+		skip_kinship = True
+		matrix,K = corr_matrix(geno_matrix,skip_kinship)
+	else:
+		matrix,K = corr_matrix(geno_matrix,skip_kinship)
 	#substitute matrix of imputed values with standardise matrix
 	genofile['genotype/matrix'][:]=matrix[:]
 	#populate dataset with Kpop
@@ -67,6 +83,8 @@ if __name__ == '__main__':
 	for dset in genofile.keys():
     		for key in genofile[dset].keys():
         		print key
-	print 'OK\n'
+	print 'OK'
 
 	genofile.close()
+
+	sys.exit(0)
