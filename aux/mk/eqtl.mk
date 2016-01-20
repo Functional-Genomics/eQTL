ad7478abf4682d5c6267979e7abcc0a725bd75b8
Nuno Fonseca <firstname.lastname@gmail.com>
2016-01-20 17:59:11 +0000

    Issue number 7

--- aux/mk/eqtl.mk
+++ aux/mk/eqtl.mk
@@ -2,24 +2,52 @@ ifeq ($(eqtl_method),limix)
 step4: $(step1a_dir)/complete $(step2_dir)/complete $(step3_dir)/complete $(eqtl_dir)/step4.complete
 
 ifeq ($(cis_window),0)
+############################################################
+# Limix trans-eQTL (start)
 $(info trans-EQTL mode)
-eqtl_cmd1=eqtl_trans.py
+# Trans specific
+$(step1a_dir)/all_chr.hdf5: $(foreach chr,$(chromosomes),$(step1a_dir)/$(chr)/chr$(chr).hdf5)
+	$(file >$@.lst.txt,$^) \
+	sed -i -E "s/^ //;s/ +/\n/g" $@.lst.txt && \
+	aggregate_chromosomes.py $@.lst.txt $@.tmp.hdf5 && \
+	mv $@.tmp.hdf5 $@ && rm -f $@.lst.txt
+
+# across the whole genome
+All_QTL_JOBS=$(foreach j,$(shell seq $(n_folds)), $(eqtl_dir)/all_chr/$(n_folds)_$(j).hdf5)
+
+
+$(eqtl_dir)/all_chr/$(n_folds)_%.hdf5: $(step1a_dir)/all_chr.hdf5 $(step2_dir)/$(expr_matrix_filename).filtered.hdf5  $(kpop_file) $(step3_dir)/$(corr_method)/$(corr_method).hdf5 $(cov_sorted_hdf5)
+	mkdir -p $(@D) && eqtl_trans.py $<   $(step2_dir)/$(expr_matrix_filename).filtered.hdf5  $(corr_method)  $(step3_dir)/$(corr_method)/$(corr_method).hdf5  $(kpop_file) $(cov_sorted_hdf5) $(limix_use_peer_covariates) $(cis_window) $(n_permutations) $(n_folds) $* $@.tmp && mv $@.tmp $@
+
+
+$(eqtl_dir)/all_chr/summary.hdf5: $(All_QTL_JOBS)  $(cov_sorted_hdf5) $(step2_dir)/$(expr_matrix_filename).filtered.hdf5 $(step3_dir)/$(corr_method)/$(corr_method).hdf5
+	$(file >$@.lst.txt,$(All_QTL_JOBS)) \
+	sed -i -E "s/^ //;s/ +/\n/g" $@.lst.txt && \
+	eqtl_summary.py $(step1a_dir)/all_chr.hdf5  $(step2_dir)/$(expr_matrix_filename).filtered.hdf5  $(corr_method) $(step3_dir)/$(corr_method)/$(corr_method).hdf5  $(kpop_file) $(cov_sorted_hdf5) $(cis_window) $(n_permutations) $(snp_alpha) $(n_folds) $@.lst.txt  $@.tmp && \
+	rm -f $@.lst.txt && \
+	mv $@.tmp $@
+
+$(eqtl_dir)/summary.hdf5: $(eqtl_dir)/all_chr/summary.hdf5
+	$(file >$@.lst.txt,$^) \
+	sed -i -E "s/^ //;s/ +/\n/g" $@.lst.txt && \
+	eqtl_aggregate.py $@.tmp $(cis_window) $(n_permutations) $@.lst.txt && \
+	mv $@.tmp $@ && rm -f $@.lst.txt
+
+# Limix trans-eQTL (end)
+############################################################
 else
+############################################################
+# Limix cis-eQTL (start)
 $(info cis-EQTL mode)
 eqtl_cmd1=eqtl_cis.py
-endif
+
 
 # foreach chr
 All_QTL_JOBS=$(foreach j,$(shell seq $(n_folds)),$(foreach chr,$(chromosomes), $(eqtl_dir)/$(chr)/$(n_folds)_$(j).hdf5))
 
-# $(1)=chr
 define QTL_JOBS_chr=
 $(foreach j,$(shell seq $(n_folds)), $(eqtl_dir)/$(1)/$(n_folds)_$(j).hdf5)
 endef
-
-#$(info $(All_QTL_JOBS))
-TARGETS7+=$(All_QTL_JOBS)
-
 
 
 # $(1) = chr
@@ -31,7 +59,7 @@ $(eqtl_dir)/$(1)/$(n_folds)_%.hdf5: $(step1a_dir)/$(1)/chr$(1).hdf5 $(step2_dir)
 $(eqtl_dir)/$(1).hdf5: $(call QTL_JOBS_chr,$(1))  $(cov_sorted_hdf5) $(step2_dir)/$(expr_matrix_filename).filtered.hdf5 $(step3_dir)/$(corr_method)/$(corr_method).hdf5
 	$$(file >$$@.lst.txt,$(call QTL_JOBS_chr,$(1))) \
 	sed -i -E "s/^ //;s/ +/\n/g" $$@.lst.txt && \
-	eqtl_summary.py $(step1a_dir)/$(1)/chr$(1).hdf5  $(step2_dir)/$(expr_matrix_filename).filtered.hdf5  $(corr_method) $(step3_dir)/$(corr_method)/$(corr_method).hdf5  $(kpop_file) $(cov_sorted_hdf5) $(cis_window)  $(n_folds) $$@.lst.txt  $$@.tmp && \
+	eqtl_summary.py $(step1a_dir)/$(1)/chr$(1).hdf5  $(step2_dir)/$(expr_matrix_filename).filtered.hdf5  $(corr_method) $(step3_dir)/$(corr_method)/$(corr_method).hdf5  $(kpop_file) $(cov_sorted_hdf5) $(cis_window) $(n_permutations) $(snp_alpha) $(n_folds) $$@.lst.txt  $$@.tmp && \
 	rm -f $$@.lst.txt && \
 	mv $$@.tmp $$@
 
@@ -43,14 +71,20 @@ $(foreach chr,$(chromosomes),$(eval $(call make-qtl-rule-chr,$(chr))))
 $(eqtl_dir)/summary.hdf5: $(foreach chr,$(chromosomes),$(eqtl_dir)/$(chr).hdf5)
 	$(file >$@.lst.txt,$^) \
 	sed -i -E "s/^ //;s/ +/\n/g" $@.lst.txt && \
-	eqtl_aggregate.py $@.tmp $(cis_window) $@.lst.txt && \
+	eqtl_aggregate.py $@.tmp $(cis_window) $(n_permutations) $@.lst.txt && \
 	mv $@.tmp $@ && rm -f $@.lst.txt
+
+TARGETS8+=$(foreach chr,$(chromosomes),$(eqtl_dir)/$(chr).hdf5)
+# Limix cis-eQTL (end)
+############################################################
+endif
+
+#$(info $(All_QTL_JOBS))
+TARGETS7+=$(All_QTL_JOBS)
+
 
 $(eqtl_dir)/summary.tsv: $(eqtl_dir)/summary.hdf5
 	get_results.py $(fdr_threshold) $@.tmp $^ && mv $@.tmp $@
-
-
-TARGETS8+=$(foreach chr,$(chromosomes),$(eqtl_dir)/$(chr).hdf5)
 
 else
 #######################################################
