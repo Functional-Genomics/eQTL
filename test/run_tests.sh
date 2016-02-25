f6373055ee3eba4775c4e31359bd3262dba9ca95
Nuno Fonseca <firstname.lastname@gmail.com>
2016-02-25 11:56:26 +0000

    New test. Output test results to a specific folder

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 set -e
+mkdir -p test_out
 #########################
 # QN
-../scripts/irap_qn --in ex1/pheno.tsv -o qn_test1.tsv -m ex1/sample2class.tsv
+../scripts/irap_qn --in ex1/pheno.tsv -o test_out/qn_test1.tsv -m ex1/sample2class.tsv
+
+../scripts/irap_qn --in files/expr1.fpkm.tsv.gz -o test_out/qn_expr1  -m files/expr1_map2class.tsv
