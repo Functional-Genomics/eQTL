dd767e8b2c4e2bf17974174ea772eb2b12ab417b
Nuno Fonseca <firstname.lastname@gmail.com>
2016-03-03 00:47:19 +0000

    Add snr_threshold parameter to filtering_pheno.py

--- aux/mk/rna.mk
+++ aux/mk/rna.mk
@@ -5,10 +5,10 @@ step2: $(step2_dir)/complete
 
 # Filtering the pheno matrix based on fpkm threshold and number of samples
 $(step2_dir)/$(expr_matrix_filename).filtered.tsv: $(matched_expr_matrix)
-	filtering_pheno.py $< $(min_expr) $(min_perc_samples) $@.tmp && mv $@.tmp $@
+	filtering_pheno.py $< $(min_expr) $(min_perc_samples) $(snr_threshold) $@.tmp && mv $@.tmp $@
 
 # Quantile normalization per classes/studies/groups
-$(step2_dir)/$(expr_matrix_filename).filtered.qn.tsv: $(step2_dir)/$(expr_matrix_filename).filtered.tsv
+$(step2_dir)/$(expr_matrix_filename).filtered.qn.tsv: $(step2_dir)/$(expr_matrix_filename).filtered.tsv $(sample2class_file)
 	irap_qn -i $< -m $(sample2class_file) -o $@.tmp && mv $@.tmp $@
 
 # Transform expression values of each gene across all the samples.
