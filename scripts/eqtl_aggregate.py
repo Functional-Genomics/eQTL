a788f34a161b5ef72165bd06025bb5e542db6b9d
clody23 <claudia.calabrese23@gmail.com>
2016-05-02 13:57:13 +0100

    take tsv files as input and output

--- scripts/eqtl_aggregate.py
+++ scripts/eqtl_aggregate.py
@@ -12,6 +12,8 @@ from utils import getLambda
 import readline # because of an error with importing rpy2.robjects
 from rpy2.robjects.packages import importr
 from rpy2.robjects.vectors import FloatVector
+import pandas as pd
+
 
 stats = importr('stats')
 
@@ -21,101 +23,69 @@ If cis, this script generates a final hdf5 file aggregating all the chr.hdf5 fil
 
 Usage:
 
-eqtl_aggregate.py <out.summary.cis.hdf5> <window> <n_perm>  <chr.lst>
+eqtl_aggregate.py <summary.fold.tsv> <metainfo.tsv> <outfile.tsv> 
 '''
 if __name__ == '__main__':
 
-	if len(sys.argv[1:]) < 4:
+	if len(sys.argv[1:]) < 3:
 		sys.stderr.write('ERROR: missing parameters\n')
 		usage()
 		sys.exit(1)
 
-	outfile,window,n_perm = sys.argv[1:4]
-	window = float(window)
-	n_perm = int(n_perm)
-
-	with open(sys.argv[4]) as f:
-		chr = [i.strip() for i in f.readlines()]
-
-	for file in chr:
-		if os.path.isfile(file) != True:
-			sys.stderr.write('ERROR: file '+file+' not found\n')
-			sys.exit(1)
-
-	if type(window) != (int) and  type(window) != (float):
-		sys.stderr.write('\nERROR: Please use integer or float for window\n')
-		usage()
+	file,metainfo,outfile = sys.argv[1:]
+#	with open(file) as f:
+#		chr = [i.strip() for i in f.readlines()]	
+	if os.path.isfile(file) != True:
+		sys.stderr.write('ERROR: file '+file+' not found\n')
 		sys.exit(1)
-	elif type(n_perm) != (int):
-		sys.stderr.write('\nERROR: Please use integer for n_perm\n')
-		usage()
+	elif os.path.isfile(metainfo)!= True:
+		sys.stderr.write('ERROR: file '+metainfo+' not found\n')
 		sys.exit(1)
 
-	out = h5py.File(outfile,'w')
-	#open a dictionary
-	table = {}
-	for file in chr:
-		temp = {}
-		i = h5py.File(file,'r')
-		if i.keys() == []: #the file is empty
-			sys.stdout.write('WARNING: file {0} is empty\n'.format(i))
-			continue
-		else:
-			temp['geneID'] =i['geneID'][:]
-			temp['file'] = i['file'][:]
-			temp['chrom'] = i['chrom'][:]
-			temp['pos'] = i['pos'][:]
-			temp['pval'] = i['pv'][:]
-			temp['l_adj_pval'] = i['qv'][:]
-			temp['beta'] = i['beta'][:]
-			temp['lambda_pval'] = i['lambda'][:]
-			temp['lambda_perm'] = i['lambda_perm'][:]
-			temp['l_emp_pval'] = i['pv_perm'][:]
-			if window == 0:
-				temp['n_tests'] = i['n_tests'][:] #if trans
-		#append all file groups to the big table
-		for key in temp.keys():
-			smartAppend(table,key,temp[key])
-	
-	for key in table.keys():
-		try:
-			table[key] = sp.concatenate(table[key])
-		except:
-			pass
-	if table.keys() == []:
-		sys.stdout.write('WARNING: no result found. Writing an empty {0} file\n'.format(outfile))
-		out.close()
+	file = pd.read_csv(file,sep='\t')
+	metainfo = pd.read_csv(metainfo,sep='\t',index_col=[0],header=None)
+
+	n_tests = int(metainfo[metainfo.index == 'n_tests'].values[0][0])
+	window = float(metainfo[metainfo.index == 'window'].values[0][0])
+	n_genes = int(metainfo[metainfo.index == 'Number_of_phenotypesID_tested'].values[0][0])
+	n_perm = int(metainfo[metainfo.index == 'n_perm'].values[0][0])
+	#open out tsv file
+	out = open(outfile,'w')
+	if file.shape[0] == 1:
+		sys.stdout.write('WARNING: file {0} is empty\n'.format(i))
 	else:
+		pval = file['pv'].astype(float)
+#		l_adj_pval = file['qv'].astype(float)
+#		beta = file['beta'].astype(float)
+#		lambda_pval= file['lambda'].astype(float)
+#		lambda_perm= file['lambda_perm'].astype(float)
+#		l_emp_pval = file['pv_perm'].astype(float)
 		#store the number of pvalues tested
-		shape_pv = table['pval'].shape[0]
+		shape_pv = pval.shape[0]
 		#calculate qv_all,pv_perm_all
 		if window != 0 and n_perm <=1: #if cis and no empirical pvalues
-			table['g_adj_pval'] = FDR.qvalues(table['l_adj_pval'])
-			table['window'] = [window] #add group with window size
-			table['n_perm'] = [n_perm]
-			table['g_emp_adj_pval'] = (sp.empty((shape_pv,))).astype(str)
-			table['g_emp_adj_pval'][:] = 'NA' #fill an empty array with NA values
+			g_adj_pval = FDR.qvalues(file['qv'].astype(float),m=n_genes)
+			g_emp_adj_pval = (sp.empty((shape_pv,))).astype(str)
+			g_emp_adj_pval[:] = 'NA' #fill an empty array with NA values
 		elif window !=0 and n_perm >1: # if cis and empirical pvalues
-			table['g_adj_pval'] = FDR.qvalues(table['l_adj_pval'])
-			table['g_emp_adj_pval'] = FDR.qvalues(table['l_emp_pval'][:,0])
-			table['window'] = [window]
-			table['n_perm'] = [n_perm]
+			g_adj_pval = FDR.qvalues(file['qv'].astype(float),m=n_genes)
+			g_emp_adj_pval = FDR.qvalues(file['pv_perm'].astype(float),m=n_genes)
 		elif window == 0 and n_perm <=1: #if trans and no empirical pvalues
-			table['g_adj_pval'] = sp.array(stats.p_adjust(FloatVector(table['pval'][:].tolist()),method = 'bonferroni',n=table['n_tests'][:][0])) #compute bonferroni adjusted across nominal pvalues
-			table['window'] = [window]
-			table['n_perm'] = [n_perm]
-			table['g_emp_adj_pval'] = (sp.empty((shape_pv,))).astype(str) #fill an empty array with NA values
-			table['g_emp_adj_pval'][:] = 'NA'
+			pval = file['pv'].astype(float)
+			g_adj_pval = sp.array(stats.p_adjust(FloatVector(pval.tolist()),method = 'bonferroni',n=float(n_tests))) #compute bonferroni adjusted across nominal pvalues
+			g_emp_adj_pval = (sp.empty((shape_pv,))).astype(str) #fill an empty array with NA values
+			g_emp_adj_pval[:] = 'NA'
 		else: #if trans and empirical pvalues
-			table['g_adj_pval'] = (sp.empty((shape_pv,))).astype(str) #fill an empty array with NA values
-			table['g_adj_pval'] [:] = 'NA'
-			table['g_emp_adj_pval'] = sp.array(stats.p_adjust(FloatVector(table['l_emp_pval'][:,0].tolist()),method = 'bonferroni',n=table['n_tests'][:][0])) #compute bonferroni adjusted across empirical pvalues
-			table['window'] = [window]
-			table['n_perm'] = [n_perm]
+			g_adj_pval = (sp.empty((shape_pv,))).astype(str) #fill an empty array with NA values
+			g_emp_adj_pval= sp.array(stats.p_adjust(FloatVector((file['pv_perm'].astype(float)).tolist()),method = 'bonferroni',n=float(n_tests))) #compute bonferroni adjusted across empirical pvalues
+	file.insert(0,'g_adj_pval',pd.DataFrame(g_adj_pval))
+	file.insert(0,'g_emp_adj_pval',pd.DataFrame(g_emp_adj_pval))
+	file = file[['geneID','chrom','pos','pv','pv_perm','g_emp_adj_pval','qv','g_adj_pval',	'beta','lambda','lambda_perm','file']]
+	file.rename(columns={'pv':'pval','pv_perm':'l_emp_pval','qv':'l_adj_pval','lambda':'lambda_pval'},inplace=True)
+	file.to_csv(out,sep='\t',header=True,index=None,na_rep='NA')
+	out.close()	
+	
 
-		#write within the file		
-		smartDumpDictHdf5(table,out)
-		out.close()
 	sys.exit(0)
 
 		
