68c2668bf26641b96eedf0e687af043c3b54df4d
Nuno Fonseca <firstname.lastname@gmail.com>
2016-06-23 16:05:57 +0100

    add rule to convert panama.hdf5 to tsv

--- aux/mk/correction.mk
+++ aux/mk/correction.mk
@@ -58,8 +58,8 @@ $(step3_dir)/none/none.tsv: $(step3_dir)/none/none.hdf5
 $(step3_dir)/peer/peer.tsv: $(step3_dir)/peer/peer.hdf5
 	hdf52tsv $< "/phenotype" "/row_header/sample_ID" "/col_header/phenotype_ID" $@.tmp y && mv $@.tmp $@
 
-# $(step3_dir)/panama/panama.tsv: $(step3_dir)/panama/panama.hdf5
-# 	hdf52tsv $< "Ktot" "/row_header/sample_ID" "/col_header/sample_ID" $@.tmp y && mv $@.tmp $@
+$(step3_dir)/panama/panama.tsv: $(step3_dir)/panama/panama.hdf5
+	hdf52tsv $< "Ktot" "/row_header/sample_ID" "/col_header/sample_ID" $@.tmp y && mv $@.tmp $@
 
 
 ##########################################
