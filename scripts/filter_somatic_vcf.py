6c30b38beff87a751849ebded6543a2c5366f403
clody23 <claudia.calabrese23@gmail.com>
2016-09-30 18:44:18 +0100

    filter merged somatic vcfs by VAF. Called by vcf_filter_cmd

--- scripts/filter_somatic_vcf.py
+++ scripts/filter_somatic_vcf.py
@@ -5,15 +5,16 @@ import sys, os
 
 def usage():
 	print """
-This script filters by VAF and LOWSUPPORT and adds one new column to the original vcf file with the VAF value. 
+This script filters by VAF and LOWSUPPORT and adds one new column to the original vcf file with the VAF value and print to stdout
 
 Usage:
-filter_somatic_merged_vcf.py <file.vcf> <VAF_threshold <chr> > filtered.vcf
+filter_somatic_merged_vcf.py <file.vcf.gz> <VAF_threshold> <chr> <use_pass> > filtered.vcf
+use_pass= [y | n]
 """
 
 
 if __name__ == "__main__":
-	if len(sys.argv[1:]) < 3:
+	if len(sys.argv[1:]) < 4:
 		sys.stderr.write('\nERROR: missing argument')
 		usage()
 		sys.exit(1)
@@ -28,6 +29,12 @@ if __name__ == "__main__":
 		sys.exit(1)
 	
 	chromosome=str(sys.argv[3])
+	try:
+		use_pass=str(sys.argv[4])
+	except:
+		sys.stderr.write('ERROR: please provide y or n for use_pass parameter\n')
+		sys.exit(1)
+
 	for line in fileinput.input(sys.argv[1],openhook=fileinput.hook_compressed):
 		if line.startswith('#'):
 			print line.strip()
@@ -38,18 +45,20 @@ if __name__ == "__main__":
 				continue
 			else:
 				filter=lines[-2]
-				if filter == 'LOWSUPPORT':
-					continue
+				if use_pass == 'y':
+					if filter == 'LOWSUPPORT':
+						continue
 				else:
-					info=lines[-1]
-					info=info.split(';')
-					for v in info:
-						if 'VAF' in v:
-							AF = float(v.split('=')[-1])
-							if AF>= threshold:
-								new_list=line.strip()+'\t'+str(AF)
-								print new_list
-							else:
-								continue
+					pass
+				info=lines[-1]
+				info=info.split(';')
+				for v in info:
+					if 'VAF' in v:
+						AF = float(v.split('=')[-1])
+						if AF>= threshold:
+							new_list=line.strip()
+							sys.stdout.write(new_list+'\n')
+						else:
+							continue
 			
 	sys.exit(0)	
