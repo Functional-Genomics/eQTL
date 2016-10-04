51d529285ac6dfb61f75409ac7478508135dc9ca
clody23 <claudia.calabrese23@gmail.com>
2016-10-04 18:06:55 +0100

    use_kinship added

--- scripts/eqtl_trans.py
+++ scripts/eqtl_trans.py
@@ -23,40 +23,46 @@ This script runs the eqtl analysis on a chunk of the gene expression matrix VS a
 
 Usage:
 
-eqtl_trans.py <allchr.hdf5> <pheno.filtered.hdf5> <peer> <peer.hdf5> <Kpop.hdf5> <covariates.hdf5> <peer_cov> <cis_window> <n_perm> <change_beta_sign> <nfolds> <fold_j> <outfilename>
+eqtl_trans.py <allchr.hdf5> <pheno.filtered.hdf5> <peer> <peer.hdf5> <Kpop.hdf5> <covariates.hdf5> <use_kinship> <peer_cov> <cis_window> <n_perm> <change_beta_sign> <nfolds> <fold_j> <outfilename>
 
-peer_cov values = n | y [default=n] '''
+peer_cov values = n | y [default=n] 
+use_kinship = n| y
+'''
 
-def run_lmm(booleanK,peer_cov,Xc,Y,cov,K):
+def run_lmm(use_kinship,peer_cov,Xc,Y,cov,K):
         '''this functions computes the lmm model using X (genotype), Y(phenotype). Optional: cov(matrix of covariance), K(kinship)'''
-	if True in booleanK:
+	if use_kinship=='y':
 		if peer_cov =='n': #if no covariates were used with peer then account for cov in the model
-			lmm = QTL.test_lmm(Xc,Y,covs=cov)
+			sys.stderr.write('\nrunning lmm = QTL.test_lmm(Xc,Y,covs=cov,K=K)...')
+			lmm = QTL.test_lmm(Xc,Y,covs=cov,K=K)
 		else:
-			lmm = QTL.test_lmm(Xc,Y) # otherwise exclude covariates from the model since already used in peer
-	else:
+			sys.stderr.write('\nrunning lmm = QTL.test_lmm(Xc,Y,K=K)...')
+			lmm = QTL.test_lmm(Xc,Y,K=K) # otherwise exclude covariates from the model since already used in peer
+	else: #no kinship
 		if peer_cov =='n': #if no cov where used with peer then
-			lmm = QTL.test_lmm(Xc,Y,covs=cov,K=K) #use cov in the model
+			sys.stderr.write('\nrunning lmm= QTL.test_lmm(Xc,Y,covs=cov,K=SP.eye(Xc.shape[0]))...')
+			lmm = QTL.test_lmm(Xc,Y,covs=cov,K=SP.eye(Xc.shape[0])) #include covariates in the model
 		else:
-			lmm = QTL.test_lmm(Xc,Y,K=K) #exclude cov in the model since already used by peer
+			sys.stderr.write('\nrunning lmm = QTL.test_lmm(Xc,Y,K=SP.eye(Xc.shape[0]))...')
+			lmm = QTL.test_lmm(Xc,Y,K=SP.eye(Xc.shape[0])) #exclude cov in the model since already used by peer
 	return lmm
 
 
-if len(sys.argv[1:]) < 13:
+if len(sys.argv[1:]) < 14:
 	usage()
 	sys.stderr.write('ERROR: missing parameters\n')
 	sys.exit(1)
 
 #read args
-geno,pheno,cm,cm_hdf5,kinship,cov_hdf5,peer_cov,window,n_perm,change_beta_sign = sys.argv[1:11]
+geno,pheno,cm,cm_hdf5,kinship,cov_hdf5,use_kinship,peer_cov,window,n_perm,change_beta_sign = sys.argv[1:12]
 window=float(window)
 n_perm=int(n_perm)
 
 #populate dictionary with all the data needed for eqtl analysis
 
 #take nfold and j to name the out file for each j
-nfolds = int(sys.argv[11])
-fold_j = int(sys.argv[12])
+nfolds = int(sys.argv[12])
+fold_j = int(sys.argv[13])
 
 
 if type(nfolds) != (int):
@@ -74,10 +80,17 @@ elif type(window) != (float) and type(window) != (int):
 elif change_beta_sign != 'y' and change_beta_sign != 'n':
 	usage()
 	sys.stderr.write('\nERROR: Please use either y or n for beta sign option\n')
-	sys.exit(1) 
+	sys.exit(1)
+elif use_kinship !='y' and use_kinship !='n':
+	usage()
+	sys.stderr.write('\nERROR: Please use either y or n for use_kinship\n')
+	sys.exit(1)
 
 #open outfile 
-fout  = h5py.File(sys.argv[13],'w')  #%d_%.3d.hdf5'%(nfolds,fold_j) #this should take as argument a name like nfolds_j.hdf5
+fout  = h5py.File(sys.argv[14],'w')  #%d_%.3d.hdf5'%(nfolds,fold_j) #this should take as argument a name like nfolds_j.hdf5
+
+sys.stderr.write('\nParameters set in the association analysis:\ngenotype={0}; phenotype={1}; correction_metho={2}; correction_method_file={3}; Kinship_file={4}; covariates_file={5}; use_kinship={6}; peer_covariates={7}; cis_window={8}; number_permutation={9}; change_beta_sign={10}; n_folds={11}; fold_j={12}; outfile={13}'.format(geno,pheno,cm,cm_hdf5,kinship,cov_hdf5,use_kinship,peer_cov,window,n_perm,change_beta_sign,nfolds,fold_j,fout))
+
 # load data 
 import data as DATA
 data  = DATA.data(geno,kinship,pheno,cov_hdf5,cm_hdf5,cm,window)
@@ -97,6 +110,12 @@ Icv = SP.floor(nfolds*SP.arange(n_genes)/n_genes)
 I = Icv==fold_j
 #grab the genes in chunk j
 genes = list(genes[I])
+#output warning if NAN in Kinship
+if use_kinship=='y':
+	booleanK=SP.isnan(K)
+	if True in booleanK:
+		sys.stderr.write('\nWARNING: kinship matrix contains NA\n')
+
 #set progress bar
 bar = progressbar.ProgressBar(maxval=n_perm, widgets=[progressbar.Bar('=', '[', ']'), ' ', progressbar.Percentage()])
 #execute analysis for each gene in chunk j
@@ -115,10 +134,9 @@ for gene in genes:
 #		print "...excluding gene %s %s" %(gene,e)
 
         print "+"
-        #check if Kpop or Ktot contains Nan
-	booleanK=SP.isnan(K)
+
  	#run the linear mixed model to get nominal pvalues
-	lmm = run_lmm(booleanK,peer_cov,Xc,Y,cov,K)
+	lmm = run_lmm(use_kinship,peer_cov,Xc,Y,cov,K)
 	pv = lmm.getPv() #store nominal pv
 	RV={} #open empty dictionary to store results
 	
@@ -132,7 +150,7 @@ for gene in genes:
 		for perm_i in xrange(int(n_perm)):
 			idx = SP.random.permutation(Xc.shape[0]) #take indexes
 			Xc_perm = Xc[idx,:] #shuffle the samples of the genome matrix
-			lmm_perm =run_lmm(booleanK,peer_cov,Xc_perm,Y,cov,K) #run the lmm model on permuted genotype
+			lmm_perm =run_lmm(use_kinship,peer_cov,Xc_perm,Y,cov,K) #run the lmm model on permuted genotype
 			pv_perm = lmm_perm.getPv()# get pvalues 
 			pv0_min = pv_perm[0,:].min() #get minimum permuted pvalue
 			if pv0_min <= pv[:].min():
@@ -145,7 +163,7 @@ for gene in genes:
 		print 'number of permutations is set = 1; empirical pvalues will not be computed.'
 		idx = SP.random.permutation(Xc.shape[0]) #take indexes
 		Xc_perm = Xc[idx,:] #shuffle the samples of the genome matrix
-		lmm_perm =run_lmm(booleanK,peer_cov,Xc_perm,Y,cov,K)
+		lmm_perm =run_lmm(use_kinship,peer_cov,Xc_perm,Y,cov,K)
 
 	# run the linear mixed model
 	RV['pv'] = pv
